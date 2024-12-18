22699fcfa0d175dd04bc31249febad95d2a88928
ShikHar-166 <khadkashikhar321@gmail.com>
2024-12-18 08:02:14 +0545

    Add project files.

---
+++ 2023EMVCWebApi/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using _22023EMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2023EMVCWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+    }
+}
---
+++ 2023EMVCWebApi/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2023EMVCWebApi.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class StudentController : Controller
+    {
+        public StudentController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = new List<Student>();
+            var nirnaya = new Student { Id = 1, Name = "Nirnaya", Address = "Birgunj" };
+            var samip = new Student { Id = 2, Name = "Samip", Address = "Birgunj" };
+            students.Add(nirnaya);
+            students.Add(samip);
+            return Ok(students);
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+}
