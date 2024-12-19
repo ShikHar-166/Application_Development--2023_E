fe1f16a388a89a9ef3556428cefab9acee8e6d35
ShikHar-166 <khadkashikhar321@gmail.com>
2024-12-19 10:39:44 +0545

    Delete 22023EMVC directory

--- 22023EMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _22023EMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
